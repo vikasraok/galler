4d830b3de0a66f9003768369684f316224df2d8c
vikaraok <vikasrao1@gmail.com>
2017-03-25 17:37:35 +0530

    deployment screen with api

--- js/deploy.js
+++ js/deploy.js
@@ -1,41 +1,68 @@
 /**
  * Created by vikas on 25-Mar-17.
  */
-(function(){
-$(document).on('ready',function(){
-    $.ajax({
-        url : window.location.origin+'/galler/xml/list?type=rtu',
-        headers : {
-            'TOKEN' : $.cookie('token')
-        },
-        method : 'GET',
-        success: function (data) {
-            var d = ''
-            for (var i = 0; i < data.list.length; i++){
-                d += "<tr><td>" + data.list[i] + "</td></tr>"
-            }
-            $("#rtu-table").html(d)
-        },
-        error: function (textStatus, errorThrown) {
-            alert('couldn\'t get rtus, check logs!');
-        }
-    });
-    $.ajax({
-        url : window.location.origin+'/galler/xml/list?type=xml',
-        headers : {
-            'TOKEN' : $.cookie('token')
-        },
-        method : 'GET',
-        success: function (data) {
-            var d = ''
-            for (var i = 0; i < data.list.length; i++){
-                d += "<tr><td>" + data.list[i] + "</td></tr>"
-            }
-            $("#xml-table").html(d)
-        },
-        error: function (textStatus, errorThrown) {
-            alert('couldn\'t get rtus, check logs!');
-        }
-    })
-})
-})();
+(
+    function () {
+        $(document).on('ready', function () {
+            $.ajax({
+                url: window.location.origin + '/galler/xml/list?type=rtu',
+                headers: {
+                    'TOKEN': $.cookie('token')
+                },
+                method: 'GET',
+                success: function (data) {
+                    var d = '';
+                    for (var i = 0; i < data.list.length; i++) {
+                        $("#rtu-select").append($('<option>', {
+                            value: data.list[i],
+                            text: data.list[i]
+                        }))
+                    }
+                },
+                error: function (textStatus, errorThrown) {
+                    alert('couldn\'t get rtus, check logs!');
+                }
+            });
+            $.ajax({
+                url: window.location.origin + '/galler/xml/list?type=xml',
+                headers: {
+                    'TOKEN': $.cookie('token')
+                },
+                method: 'GET',
+                success: function (data) {
+                    var d = '';
+                    for (var i = 0; i < data.list.length; i++) {
+                        $("#xml-select").append($('<option>', {
+                            value: data.list[i],
+                            text: data.list[i]
+                        }))
+                    }
+                },
+                error: function (textStatus, errorThrown) {
+                    alert('couldn\'t get rtus, check logs!');
+                }
+            });
+            $('#deploy').click(
+                function (event) {
+                    event.preventDefault();
+                    $.ajax({
+                        url: window.location.origin + '/galler/event',
+                        headers: {
+                            'TOKEN': $.cookie('token')
+                        },
+                        method: 'POST',
+                        data: {
+                            rtu_name: $('#rtu-select').val(),
+                            file_name: $('#xml-select').val()
+                        },
+                        success: function (data) {
+                            alert('')
+                        },
+                        error: function (textStatus, errorThrown) {
+                            alert('couldn\'t get rtus, check logs!');
+                        }
+                    })
+                }
+            )
+        })
+    })();
