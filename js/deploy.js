38bfefb8e1a551839e58f4170c032b8da4a7d1a8
Ashmeet Singh <hackercomapny@gmail.com>
2017-03-25 17:20:59 +0530

    JQ

--- js/deploy.js
+++ js/deploy.js
@@ -6,16 +6,15 @@ $(document).on('ready',function(){
     $.ajax({
         url : window.location.origin+'/galler/xml/list?type=rtu',
         headers : {
-            TOKEN : $.cookie('token')
+            'TOKEN' : $.cookie('token')
         },
         method : 'GET',
         success: function (data) {
-            $.each(JSON.parse(data.list), function(i, item) {
-                var $tr = $('<tr>').append(
-                    $('<td>').text(item)
-                );
-                $tr.appendTo('#rtu-table');
-            });
+            var d = ''
+            for (var i = 0; i < data.list.length; i++){
+                d += "<tr><td>" + data.list[i] + "</td></tr>"
+            }
+            $("#rtu-table").html(d)
         },
         error: function (textStatus, errorThrown) {
             alert('couldn\'t get rtus, check logs!');
@@ -24,16 +23,15 @@ $(document).on('ready',function(){
     $.ajax({
         url : window.location.origin+'/galler/xml/list?type=xml',
         headers : {
-            TOKEN : $.cookie('token')
+            'TOKEN' : $.cookie('token')
         },
         method : 'GET',
         success: function (data) {
-            $.each(JSON.parse(data.list), function(i, item) {
-                var $tr = $('<tr>').append(
-                    $('<td>').text(item)
-                );
-                $tr.appendTo('#xml-table');
-            });
+            var d = ''
+            for (var i = 0; i < data.list.length; i++){
+                d += "<tr><td>" + data.list[i] + "</td></tr>"
+            }
+            $("#xml-table").html(d)
         },
         error: function (textStatus, errorThrown) {
             alert('couldn\'t get rtus, check logs!');
